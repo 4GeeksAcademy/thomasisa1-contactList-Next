4f324569a5efa3f9fadf8461fbe469532d297e8d
thomasisa1 <thomasisa1@users.noreply.github.com>
2024-06-04 12:35:09 -0400

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,21 @@
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import React,{useEffect} from "react"
+	import { formatPostcssSourceMap } from "vite";
 
 export const Home = () => {
-
-  const {store, dispatch} =useGlobalReducer()
-
+	let APIURL= "https://playground.4geeks.com/contact/agendas/thomasisa1/contacts"
+let contacts = []
+useEffect (() =>{
+	fetch(store.APIURL)
+	.then(response=>response.json())
+	.then(data =>{
+		console.log(data)
+		contacts = data.contacts})
+	.catch(error =>console.log(error))
+},[])
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
 		</div>
 	);
 }; 
